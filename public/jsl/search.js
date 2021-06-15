6e56a21408a4d4e4f305761a423a3747707d9892
Алексей Леонардович Рокин <rokin@finupravlenie.ru>
2021-06-15 12:34:12 +0500

    Initial commit: improved sample #6 2021-06-15

---
+++ public/jsl/search.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let panelVisibleState = false;
+
+function toggleSearchPanel(src, idx) {
+	let fm = src.closest(`form#search${idx}`);
+	let tf = fm.querySelector(`input#search__text${idx}`);
+
+	if (!panelVisibleState) {
+		tf.classList.remove('sp__hidden');
+		tf.classList.add('sp__visible');
+	}
+
+	return 0;
+}
