145f3346ada81c9960f91e90f6fcba961f6295d9
merryroger <merry_roger@yahoo.com>
2022-06-08 22:06:09 +0500

    E-mail changed and Yandex.Metrics added 2022-06-08

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -15,7 +15,7 @@
     <article class="footer foot__contacts">
         <p>Адрес: 640020, г.&nbsp;Курган, ул.&nbsp;Советская, д.&nbsp;69.</p>
         <p>Тел./факс: +7&nbsp;(3522)&nbsp;46-02-46, 46-02-08.</p>
-        <p>E-mail: <a href="mailto:npckurgan@yandex.ru">npckurgan@yandex.ru</a>.</p>
+        <p>E-mail: <a href="mailto:npc45ru@yandex.ru">npc45ru@yandex.ru</a>.</p>
     </article>
     <article class="footer foot__links">
         @foreach($menu['extra'] as $item)
@@ -31,4 +31,25 @@
             <a href="/">{{ @trans("menu.gohome") }}</a>
         @endif
     </article>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function (m, e, t, r, i, k, a) {
+            m[i] = m[i] || function () {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(89141211, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true
+        });
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/89141211" style="position:absolute; left:-9999px;" alt=""/></div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
 </footer>
