7d42978f33d3f5a641638105080191b518e052d7
merryroger <merry_roger@yahoo.com>
2022-10-11 22:17:12 +0500

    Lead article changed 2022-10-11

--- resources/views/templates/guest/homepage/banners.blade.php
+++ resources/views/templates/guest/homepage/banners.blade.php
@@ -9,6 +9,8 @@
             <a href="http://www.kgsu.ru/" class="banner__cell"><img src="/images/banners/kgu.png"></a>
             <a href="https://docs.google.com/spreadsheets/d/1x5nTY7pEW4f88yzNRNfrid0vKNZq5HtF/edit?usp=sharing&ouid=107873831220831438429&rtpof=true&sd=true"
                class="banner__cell"><img src="/images/banners/ww2_mems.png"></a>
+            <a href="https://docs.google.com/spreadsheets/d/1tIkf2NTX3LgHxB5usegb8WsJ7okFFHg0ric1jF5zWkQ/edit?usp=sharing"
+               class="banner__cell"><img src="/images/banners/okn.png"></a>
         </div>
     </div>
     <div class="banner__ctrls scroll__right" onclick="bannerCarousel.scrollRight()"></div>
