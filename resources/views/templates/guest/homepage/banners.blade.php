c06d2157e1715cdfcda0915323a276b6679eafac
merryroger <merry_roger@yahoo.com>
2022-10-15 09:53:11 +0500

    Banner set updated 2022-10-15

--- resources/views/templates/guest/homepage/banners.blade.php
+++ resources/views/templates/guest/homepage/banners.blade.php
@@ -2,7 +2,6 @@
     <div class="banner__ctrls scroll__left" onclick="bannerCarousel.scrollLeft()"></div>
     <div class="banner__band__pad">
         <div class="banner__band">
-            <a href="https://www.gosuslugi.ru/" class="banner__cell"><img src="/images/banners/gosuslugi.png"></a>
             <a href="http://podvignaroda.ru/?#tab=navHome" class="banner__cell"><img
                         src="/images/banners/heroic.jpg"></a>
             <a href="https://pamyat-naroda.ru/" class="banner__cell"><img src="/images/banners/memory.jpg"></a>
@@ -10,7 +9,8 @@
             <a href="https://docs.google.com/spreadsheets/d/1x5nTY7pEW4f88yzNRNfrid0vKNZq5HtF/edit?usp=sharing&ouid=107873831220831438429&rtpof=true&sd=true"
                class="banner__cell"><img src="/images/banners/ww2_mems.png"></a>
             <a href="https://docs.google.com/spreadsheets/d/1tIkf2NTX3LgHxB5usegb8WsJ7okFFHg0ric1jF5zWkQ/edit?usp=sharing"
-               class="banner__cell"><img src="/images/banners/okn.png"></a>
+               class="banner__cell"><img src="/images/banners/memorials_45.png"></a>
+            <a href="https://www.gosuslugi.ru/" class="banner__cell"><img src="/images/banners/gosuslugi.png"></a>
         </div>
     </div>
     <div class="banner__ctrls scroll__right" onclick="bannerCarousel.scrollRight()"></div>
